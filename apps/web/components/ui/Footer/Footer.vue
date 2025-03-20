4bd8d5b5ef18753589fb620f96c5be0c4f85cd69
Daniel Kaschlaw <d.kaschlaw@frontend-studios.de>
2025-03-20 15:01:51 +0100

    Script Issue

--- apps/web/components/ui/Footer/Footer.vue
+++ apps/web/components/ui/Footer/Footer.vue
@@ -190,30 +190,4 @@ const { simplifiedFooter = false } = defineProps<FooterProps>();
 
 const localePath = useLocalePath();
 const NuxtLink = resolveComponent('NuxtLink');
-
-!(function (e, t, n, c, r, a, i) {
-  (e.Newsletter2GoTrackingObject = r),
-    (e[r] =
-      e[r] ||
-      function () {
-        (e[r].q = e[r].q || []).push(arguments);
-      }),
-    (e[r].l = 1 * new Date()),
-    (a = t.createElement(n)),
-    (i = t.getElementsByTagName(n)[0]),
-    (a.async = 1),
-    (a.src = c),
-    i.parentNode.insertBefore(a, i);
-})(window, document, 'script', 'https://static.newsletter2go.com/utils.js', 'n2g');
-var config = {
-  container: { type: 'div', class: '', style: '' },
-  row: { type: 'div', class: '', style: 'margin-top: 15px;' },
-  columnLeft: { type: 'div', class: '', style: '' },
-  columnRight: { type: 'div', class: '', style: '' },
-  label: { type: 'label', class: '', style: '' },
-};
-n2g('create', 'uwzoausj-nxdo3t8z-8xn');
-n2g('subscribe:createForm', config, 'footer_newsletter2go');
-
-// Füge hier weitere Aktionen hinzu
 </script>
