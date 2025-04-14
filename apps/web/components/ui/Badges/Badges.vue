ba9af3915b9845fc2031c0e7d68201f5276f5c94
Daniel Kaschlaw <d.kaschlaw@frontend-studios.de>
2025-04-14 13:10:25 +0200

    Availability Test 2

--- apps/web/components/ui/Badges/Badges.vue
+++ apps/web/components/ui/Badges/Badges.vue
@@ -29,9 +29,31 @@
           :class="[
             'availability-' + product.variation.availability.id,
             { 'text-primary-500': product.variation.availability.id === 1 },
+            { 'text-yellow-500': product.variation.availability.id === 2 },
+            { 'text-orange-500': product.variation.availability.id === 3 },
+            { 'text-red-500': product.variation.availability.id === 4 },
+            { 'text-black': product.variation.availability.id === 5 },
           ]"
         />
-        {{ productGetters.getAvailabilityName(product) }}
+        <span class="hidden md:inline font-bold text-secondary-500">
+          {{
+            [1].includes(product.variation.availability.id)
+              ? 'Auf Lager'
+              : [2].includes(product.variation.availability.id)
+                ? 'Kurzfristig verfügbar'
+                : [3].includes(product.variation.availability.id)
+                  ? 'Lieferzeit ca. 2 Wochen'
+                  : [4].includes(product.variation.availability.id)
+                    ? 'Lieferzeit ca. 4 Wochen'
+                    : [5].includes(product.variation.availability.id)
+                      ? 'Auf Anfrage'
+                      : [6].includes(product.variation.availability.id)
+                        ? 'Nicht auf Lager'
+                        : [7, 8].includes(product.variation.availability.id)
+                          ? 'Vergriffen'
+                          : product.variation.availability.names.name
+          }}
+        </span>
       </li>
     </ul>
   </div>
